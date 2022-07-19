b5ca6c6256fc489162c5ad15576852defb637034
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-07-19 16:02:06 +0200

    Removed temp DOM elements

--- src/assets/components/mainContent/MainContent.tsx
+++ src/assets/components/mainContent/MainContent.tsx
@@ -10,10 +10,6 @@ function MainContent() {
     <div className={styles['main-content-wrapper']}>
       <Hero />
       <DynamicContent />
-      <h1>H1 text style</h1>
-      <h2>H2 text style</h2>
-      <h3>H3 text style</h3>
-      <p>P text style</p>
     </div>
   );
 };
