9729190c8c483586706b9858dd52ca32a363de6f
LarsGKodehode <104063134+LarsGKodehode@users.noreply.github.com>
2022-07-20 09:34:54 +0200

    Dynamic content (#2)
    * created files and added to parent component
    
    * extracted properties out of ContentCard
    
    currently inserting them through parent component
    
    * Dynamically generation and inserting of cards up and running
    
    Currently passing hardcoded info as prop.
    
    Typescript complains on props object lacking a type interface, do we need to extend React props object? or can we  our own
    
    * Cleanup
    
    moved all variables into populate function
    
    exchanged unnecessarily complext .map() and create Array with a simple for.. loop.
    
    * populateWtihCards() now typed
    
    * hacked together a solution for lacking typescript interface in ContentCard.tsx
    
    this needs a better solution for larger codebases, works for now
    
    * Removed temp DOM elements
    
    * basic card format up and running
    
    * changed to more sematically meaningfull html tags
    
    * Corrected comment
    
    * Card grid now responsive

--- src/assets/components/mainContent/MainContent.tsx
+++ src/assets/components/mainContent/MainContent.tsx
@@ -1,5 +1,6 @@
 // Sub components
 import Hero from '../subComponents/hero/Hero'
+import DynamicContent from '../subComponents/dynamicContent/DynamicContent';
 
 // Component CSS
 import styles from './MainContent.module.css'
@@ -8,10 +9,7 @@ function MainContent() {
   return(
     <div className={styles['main-content-wrapper']}>
       <Hero />
-      <h1>H1 text style</h1>
-      <h2>H2 text style</h2>
-      <h3>H3 text style</h3>
-      <p>P text style</p>
+      <DynamicContent />
     </div>
   );
 };
---
+++ src/assets/components/subComponents/dynamicContent/DynamicContent.tsx
@@ -0,0 +1,57 @@
+// Components
+import ContentCard from '../contentCard/ContentCard';
+
+// React
+import React from 'react'; // Need access to React.Children.toArray()
+
+// Component style
+import styles from './DynamicContent.module.css';
+
+/**
+ * Generates a variable amount of info cards
+ * @param amount number of cards to return
+ * @returns JSX
+ */
+function populateWithCards(amount: number): React.ReactNode[] {
+  // Hardcoded card details
+  interface CardInfo {
+    URL: string ,
+    URLTitle: string,
+    title: string,
+    info: string,
+    imageURL: string,
+  };
+
+  const cardInfo: CardInfo = {
+    URL: `https://www.nostandards.com`,
+    URLTitle: `Happy Life`,
+    title: `This is a card`,
+    info: `All the info you never asked for`,
+    imageURL: `www.pics4life.no`,
+  };
+
+  // store return list
+  let nodeArray: React.ReactNode[] = [];
+  // populate list
+  for(let i = 0; i < amount; i++) {
+    nodeArray.push(ContentCard(cardInfo))
+  };
+
+  // return
+  return nodeArray;
+};
+
+
+
+// COMPONENT
+function DynamicContent() {
+  return(
+    <section className={styles['dynamic-content-wrapper']}>
+      <ul className={styles['list-container']}>
+        {React.Children.toArray(populateWithCards(5))}
+      </ul>
+    </section>
+  );
+};
+
+export default DynamicContent;
