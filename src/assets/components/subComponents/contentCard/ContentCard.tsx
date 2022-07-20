526a958635eead989fec70f0d7d80676c996b7ed
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-07-20 09:27:55 +0200

    Card grid now responsive

--- src/assets/components/subComponents/contentCard/ContentCard.tsx
+++ src/assets/components/subComponents/contentCard/ContentCard.tsx
@@ -1,5 +1,6 @@
 // Component style
 import styles from './ContentCard.module.css';
+import '../../../css/materials/glass.css';
 
 // Typescript temporary hack
 interface CardInfo {
@@ -15,7 +16,12 @@ function ContentCard(props: CardInfo) {
   const {URL, URLTitle, title, info, imageURL} = props;
 
   return(
-    <article className={styles['content-card-container']}>
+    <article
+    className={[
+      styles['content-card-container'],
+      'glass',
+    ].join(' ')}>
+
       <h3>{title}</h3>
       <p>{info}</p>
       <a href={URL}>{URLTitle}</a>
